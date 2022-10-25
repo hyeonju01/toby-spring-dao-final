275a5568b3ed9286a5112e336a17f9c062d28806
hyeonju01 <tangled6648@gmail.com>
2022-10-25 14:33:37 +0900

    22. 10. 25. 8. Spring 도입

--- com/likelion/dao/UserDaoFactory.java
+++ com/likelion/dao/UserDaoFactory.java
@@ -1,7 +1,12 @@
 package com.likelion.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UserDaoFactory {
-    // 조립
+    // 스프링 적용
+    @Bean
     public UserDao awsUserDao() {
         AwsConnectionMaker awsConnectionMaker = new AwsConnectionMaker();
         UserDao userDao = new UserDao(awsConnectionMaker);
